eedeb3c52af9799d019a8243944cebb7e178d758
Matthew Klein <mklein994@gmail.com>
2017-12-09 17:34:20 -0600

    Pass url parameters from .env to run function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate dotenv;
+
+use std::env;
+
+#[derive(Debug)]
+struct Config {
+    token: String,
+    lat: f64,
+    lon: f64,
+}
+
+impl Config {
+    fn new() -> Self {
+        Config {
+            token: env::var("DARKSKY_KEY").unwrap(),
+            lat: env::var("DARKSKY_LAT").unwrap().parse::<f64>().unwrap(),
+            lon: env::var("DARKSKY_LON").unwrap().parse::<f64>().unwrap(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+
+    let config = Config::new();
+
+    if let Err(e) = run(config) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<std::error::Error>> {
+    println!("config: {:?}", config);
+    Ok(())
 }
