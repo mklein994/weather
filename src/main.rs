8b5a3a4dc6737c5db3dfcc52ed6aa1309754c59e
Matthew Klein <mklein994@gmail.com>
2017-12-09 19:55:53 -0600

    Get the weather from the api

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+extern crate darksky;
 extern crate dotenv;
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio_core;
 
+use darksky::{Block, DarkskyHyperRequester, Language, Unit};
+use futures::Future;
+use hyper::client::Client;
+use hyper_tls::HttpsConnector;
 use std::env;
+use tokio_core::reactor::Core;
 
 #[derive(Debug)]
 struct Config {
@@ -31,6 +41,24 @@ fn main() {
 }
 
 fn run(config: Config) -> Result<(), Box<std::error::Error>> {
-    println!("config: {:?}", config);
+    let mut core = Core::new()?;
+    let handle = core.handle();
+
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle)?)
+        .build(&handle);
+
+    let work = client
+        .get_forecast_with_options(&config.token, config.lat, config.lon, |o| {
+            o.exclude(vec![Block::Minutely])
+                .unit(Unit::Auto)
+                .language(Language::En)
+        })
+        .and_then(move |f| {
+            println!("{:#?}", f);
+            Ok(())
+        });
+
+    core.run(work)?;
     Ok(())
 }
