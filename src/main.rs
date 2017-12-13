accab105e33ff26c243d2a3c6373d0ded5b3db8d
Matthew Klein <mklein994@gmail.com>
2017-12-12 22:19:15 -0600

    Build config path so that it works on Windows

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,16 @@ use weather::{app, Config};
 fn main() {
     env_logger::init().expect("failed to initialize logger");
 
-    dotenv::from_path(
-        env::home_dir()
-            .unwrap()
-            .join(format!(".config/{}/config", env!("CARGO_PKG_NAME")))
-            .as_path(),
-    ).unwrap_or_else(|e| {
+    let settings_path = env::home_dir()
+        .unwrap()
+        .join(".config")
+        .join(env!("CARGO_PKG_NAME"))
+        .join("config");
+
+    dotenv::from_path(settings_path.as_path()).unwrap_or_else(|e| {
         eprintln!(
-            "Error parsing config file ~/.config/{}/config: {}",
-            env!("CARGO_PKG_NAME"),
+            "Error parsing config file \"{}\": {}",
+            settings_path.display(),
             e
         );
         std::process::exit(1);
