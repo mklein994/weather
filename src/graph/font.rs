a833eff1e0e0a34eea724babf39040197c6381ab
Matthew Klein <mklein994@gmail.com>
2018-02-27 20:24:27 -0600

    Add sparks font string tests

--- src/graph/font.rs
+++ src/graph/font.rs
@@ -87,3 +87,80 @@ impl fmt::Display for Font {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_font_display_string_bar() {
+        assert_eq!(
+            "Sparks BarExtra-narrow",
+            Font::new(Style::Bar, Weight::ExtraSmall).to_string()
+        );
+        assert_eq!(
+            "Sparks BarNarrow",
+            Font::new(Style::Bar, Weight::Small).to_string()
+        );
+        assert_eq!(
+            "Sparks BarMedium",
+            Font::new(Style::Bar, Weight::Medium).to_string()
+        );
+        assert_eq!(
+            "Sparks BarWide",
+            Font::new(Style::Bar, Weight::Large).to_string()
+        );
+        assert_eq!(
+            "Sparks BarExtra-wide",
+            Font::new(Style::Bar, Weight::ExtraLarge).to_string()
+        );
+    }
+
+    #[test]
+    fn check_font_display_string_dot() {
+        assert_eq!(
+            "Sparks DotExtra-small",
+            Font::new(Style::Dot, Weight::ExtraSmall).to_string()
+        );
+        assert_eq!(
+            "Sparks DotSmall",
+            Font::new(Style::Dot, Weight::Small).to_string()
+        );
+        assert_eq!(
+            "Sparks DotMedium",
+            Font::new(Style::Dot, Weight::Medium).to_string()
+        );
+        assert_eq!(
+            "Sparks DotLarge",
+            Font::new(Style::Dot, Weight::Large).to_string()
+        );
+        assert_eq!(
+            "Sparks DotExtra-large",
+            Font::new(Style::Dot, Weight::ExtraLarge).to_string()
+        );
+    }
+
+    #[test]
+    fn check_font_display_string_bar_dotline() {
+        assert_eq!(
+            "Sparks Dot-lineExtra-thin",
+            Font::new(Style::DotLine, Weight::ExtraSmall).to_string()
+        );
+        assert_eq!(
+            "Sparks Dot-lineThin",
+            Font::new(Style::DotLine, Weight::Small).to_string()
+        );
+        assert_eq!(
+            "Sparks Dot-lineMedium",
+            Font::new(Style::DotLine, Weight::Medium).to_string()
+        );
+        assert_eq!(
+            "Sparks Dot-lineThick",
+            Font::new(Style::DotLine, Weight::Large).to_string()
+        );
+        assert_eq!(
+            "Sparks Dot-lineExtra-thick",
+            Font::new(Style::DotLine, Weight::ExtraLarge).to_string()
+        );
+    }
+}
